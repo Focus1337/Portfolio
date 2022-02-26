3fad9c323654a35e6d9b1017fef883a799b77872
Focus <maniaclord@bk.ru>
2022-02-26 20:30:01 +0300

    email send fixed

---
+++ GroupProject/Portfolio.Misc/Services/EmailSender/IEmailService.cs
@@ -0,0 +1,6 @@
+﻿namespace Portfolio.Misc.Services.EmailSender;
+
+public interface IEmailService
+{
+    void SendEmail(Message message);
+}
---
+++ GroupProject/Portfolio/Controllers/ContactController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portfolio.Misc.Services.EmailSender;
+
+namespace Portfolio.Controllers;
+
+public class ContactController : Controller
+{
+    private readonly IEmailService _emailService;
+
+    // GET
+    public ContactController(IEmailService emailService) =>
+        _emailService = emailService;
+
+    [HttpGet]
+    public IActionResult Index() =>
+        View();
+    
+    [HttpPost]
+    public IActionResult Send()
+    {
+        var message = new Message(new string[] {"maniaclord@bk.ru"}, "Test email",
+            "This is the content from our email.");
+        _emailService.SendEmail(message);
+
+        return Ok("sent successfully");
+    }
+}
