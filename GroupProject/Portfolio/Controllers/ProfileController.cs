2542a445b530fbc4bb358c41b6b4e3b21cd64a1a
Focus <maniaclord@bk.ru>
2022-03-25 00:31:46 +0300

    user profile

---
+++ GroupProject/Portfolio/Controllers/ProfileController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.Controllers;
+
+[Authorize(Roles = "user")]
+public class ProfileController : Controller
+{
+    public IActionResult Index() =>
+        View();
+    
+    [HttpGet]
+    public IActionResult Edit() =>
+        View();
+}
--- GroupProject/Portfolio/Program.cs
+++ GroupProject/Portfolio/Program.cs
@@ -44,6 +44,16 @@ if (!app.Environment.IsDevelopment())
         .UseHsts();
 }
 
+app.Use(async (context, next) =>
+{
+    await next();
+    if (context.Response.StatusCode == 404)
+    {
+        context.Request.Path = "/Home/PageNotFound";
+        await next();
+    }
+});
+
 app
     .UseHttpsRedirection()
     .UseStaticFiles()
