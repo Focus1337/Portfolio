ccfaa1a2e8b24678fbcd3fb807dcc00b113eaa89
Focus <maniaclord@bk.ru>
2022-04-09 15:14:22 +0300

    custom helpers

--- GroupProject/Portfolio.DataAccess/Migrations/20220409120613_Init.Designer.cs
+++ GroupProject/Portfolio.DataAccess/Migrations/20220409120613_Init.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Portfolio.DataAccess.Repository;
@@ -11,9 +12,10 @@ using Portfolio.DataAccess.Repository;
 namespace Portfolio.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20220409120613_Init")]
+    partial class Init
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ GroupProject/Portfolio/CustomHelpers/CustomHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Portfolio.CustomHelpers;
+
+public static class CustomHelper
+{
+    // Indian version
+    public static HtmlString SubmitButton(this IHtmlHelper htmlHelper, string name, object value, string attributes)
+    {
+        var str = $"<button type='submit' class='{attributes}' name='{name}'>{value}</button>";
+        return new HtmlString(str);
+    }
+    
+}
